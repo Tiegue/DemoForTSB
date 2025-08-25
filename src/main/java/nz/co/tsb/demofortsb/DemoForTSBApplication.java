9964643fc93c7251f895e2dc4ead266039eba376
tiegue <tiegue303@gmail.com>
2025-08-26 01:13:59 +1200

    Initial commit: Reinitialized DemoForTSB project

---
+++ src/main/java/nz/co/tsb/demofortsb/DemoForTSBApplication.java
@@ -0,0 +1,13 @@
+package nz.co.tsb.demofortsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForTSBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForTSBApplication.class, args);
+	}
+
+}
